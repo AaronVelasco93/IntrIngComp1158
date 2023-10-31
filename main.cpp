084f0f88f862d27bea592b4e841d5cf3b3b5f262
AaronVelasco93 <aaronvelasco1933@outlook.com>
2023-10-30 18:13:44 -0600

    Cambio 1

--- main.cpp
+++ main.cpp
@@ -3,5 +3,14 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
+	
+	int numero =10;
+	
+	while(numero == 10){
+		
+		printf("Hola mundo");
+	}
+	
+	
 	return 0;
 }
